024f99374c20f08574a6ecc4cf4e8ca6e873b145
Jaime <jaime5alvarez5h@gmail.com>
2025-05-01 21:07:30 +0200

    refactor

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,11 @@
-import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
+import { DATABASE_URL } from '@/config/server-constants';
 
 export default defineConfig({
   out: './src/modules/database/infrastructure/drizzle',
   schema: './src/modules/database/infrastructure/drizzle/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: DATABASE_URL!,
   },
 });
